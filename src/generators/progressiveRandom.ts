730adb7aecbe21347bb0fa65783ea10f5efb98fb
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2018-08-28 16:24:52 +0300

    Documentation for ProgressiveRandom

--- src/generators/progressiveRandom.ts
+++ src/generators/progressiveRandom.ts
@@ -1,25 +1,59 @@
 import { DataGenerator, Point } from '../data-generator'
 
-export interface ProgressiveRandomDataGeneratorProps {
+/**
+ * Options for the Progressive random data generator.
+ */
+export interface ProgressiveRandomOptions {
+    /**
+     * How many points of data to generate.
+     * Default: 1000
+     */
     numberOfPoints?: number,
+    /**
+     * How often to change the offset.
+     * Default: numberOfPoints / 10
+     */
     offsetStep?: number,
+    /**
+     * Maximum change of the offset during one step.
+     * Values higher than max are forced to max.
+     * Default: 0.1
+     * Max: 1
+     */
     offsetDeltaMax?: number,
+    /**
+     * Minimum change of the offset during one step.
+     * Values lower than min are forec to min.
+     * Default: 0.05
+     * Min: 0
+     */
     offsetDeltaMin?: number,
-    offsetRange?: number
+    /**
+     * Maximum value for the random data before addding the offset.
+     * Values higher than max are forced to max.
+     * Default: 0.3
+     * Max: 1
+     */
+    dataMax?: number
 }
 
-export class ProgressiveRandom extends DataGenerator<Point, ProgressiveRandomDataGeneratorProps> {
-    constructor( args: ProgressiveRandomDataGeneratorProps ) {
+/**
+ * Progressive random data generator.
+ */
+export class ProgressiveRandom extends DataGenerator<Point, ProgressiveRandomOptions> {
+    constructor( args: ProgressiveRandomOptions ) {
         super( args )
     }
 
-    generator( args: ProgressiveRandomDataGeneratorProps ) {
+    generator( args: ProgressiveRandomOptions ) {
         const data: Point[] = []
+
+        // Setup defaults
         const points = args.numberOfPoints || 1000
         const offsetStep = args.offsetStep || Math.floor( points / 10 )
-        const offsetDeltaMax = args.offsetDeltaMax || 0.9
-        const offsetDeltaMin = args.offsetDeltaMin === 0 ? 0 : args.offsetDeltaMin || 0.1
-        const offsetRange = args.offsetRange || 0.7
+        const offsetDeltaMax = Math.min( args.offsetDeltaMax || 0.1, 1 )
+        const offsetDeltaMin = Math.max( args.offsetDeltaMin === 0 ? 0 : args.offsetDeltaMin || 0.05, 0 )
+        const dataMax = Math.min( args.dataMax || 0.3, 1 )
 
         let offset = 0.5
         for ( let i = 0; i < points; i++ ) {
@@ -27,15 +61,16 @@ export class ProgressiveRandom extends DataGenerator<Point, ProgressiveRandomDat
                 const newOffset = Math.random() * ( offsetDeltaMax - offsetDeltaMin ) + offsetDeltaMin
                 offset = Math.random() > 0.5 ? offset + newOffset : offset - newOffset
             }
-            if ( offset + offsetRange > 1 ) {
-                offset = 1 - offsetRange
+            // Limit the offset so that data is newer lower than 0 or higher than 1
+            if ( offset + dataMax > 1 ) {
+                offset = 1 - dataMax
             } else if ( offset < 0 ) {
                 offset = 0
             }
 
             data.push( {
                 x: i,
-                y: offset + Math.random() * offsetRange
+                y: offset + Math.random() * dataMax
             } )
         }
 
